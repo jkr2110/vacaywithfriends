b19d924b08866d86bf22223fb07ee13764494a5e
Warren Reed <jkr2110@gmail.com>
2013-03-15 20:16:28 -0400

    updating routes path for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -77,7 +77,7 @@ Vacaywithfriends::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'vacations#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
